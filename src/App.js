a1593ee29a08bac709c72404a886efeea49ba493
Obanijesu <obanijesujohn45@gmail.com>
2022-10-29 01:53:21 -0600

    fixing calendar

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import Form from "./scenes/form";
 // import Geography from "./scenes/geography";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
-import Calendar from "./scenes/calendar/calendar";
+import Calendar from "./scenes/calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
